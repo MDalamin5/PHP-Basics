27206ee4eda8ceb27ad1264f4204176693bb1f3c
MDalamin5 <mdal.amin5@northsouth.edu>
2023-10-03 21:09:40 +0600

    form handler

---
+++ Week _01/Day5/index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>From Heandeling</title>
+</head>
+<body>
+    <main>
+    
+            <form action="demo/Week%20_01/Day5/formhandler.php" method="post">
+                <label for="firstname">First Name </label>
+                <br>
+                <input required type="text" id="firstname" name="firstname" placeholder="Firstname">
+                <br><br>
+                <label for="message">Write Something Here</label>
+                <br>
+                <textarea name="message" id="message" cols="30" rows="10" placeholder="Please Write Something"></textarea>
+                <br> <br>
+                <label >Pick your Eye Color</label> <br>
+                <input type="radio" id="eyecolorblue" name="eyecolor" value="Blue">
+                <label for="eyecolorblue">Blue</label>
+                <input type="radio" id="eyecolorbrown" name="eyecolor" value="brown">
+                <label for="eyecolorbrown">Brown</label>
+                <input type="radio" id="eyecolorblack" id="eyecolorblack" value="black">
+                <label for="eyecolorblack">Black</label>
+                <br><br>
+
+                <label >What Pet do you have??</label> <br>
+                <input type="checkbox" id="petcat" name="petcat" value="cat">
+                <label for="petcat">Cat</label>
+                <input type="checkbox" id="petdog" name="petdog" value="dog">
+                <label for="petdog">Dog</label>
+                <input type="checkbox" id="petwolf" id="petwolf" value="wolf">
+                <label for="eyecolorblack">Wolf</label>
+                <br><br>
+                <label for="cartype">Car Type you own:</label> <br>
+                <select name="cartype" id="cartype">
+                    <option value="none">None</option>
+                    <option value="Bangla">Bangla</option>
+                    <option value="Alion">Alion</option>
+                    <option value="Audi">Audi</option>  
+                </select>
+                <br><br>
+                <button type="submit">Send the form</button>
+
+
+                
+            
+     </form>
+ 
+    </main>
+</body>
+</html>
