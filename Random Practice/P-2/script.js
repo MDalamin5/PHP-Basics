cc1dd8bdc2c2420276e817b2b478337a11a8e937
MDalamin5 <mdal.amin5@northsouth.edu>
2023-10-09 21:20:46 +0600

    forntend design

---
+++ Random Practice/P-2/script.js
@@ -0,0 +1,10 @@
+function initAspectJS() {
+  // Your JavaScript logic can go here
+  // You can add event listeners or other functionality
+
+  // Example: Changing the text of an element
+  const textElement = document.getElementById("element-id-115-"); // Replace with the actual element's ID
+  textElement.textContent = "Welcome to Aspect";
+}
+
+// You can add more JavaScript functions and logic as needed
