32de8d5bdcf32500e9ba70186daeffd0cc5114fd
MDalamin5 <mdal.amin5@northsouth.edu>
2023-10-16 18:43:44 +0600

    database done

--- project/README.md
+++
@@ -1,3 +0,0 @@
-# BRS
-# bTRS
-# bTRS
--- project/index.php
+++ project/index.php
@@ -1,159 +1,127 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="boxicons/css/boxicons.min.css">
-    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
-    <link rel="stylesheet" href="style.css">
-    <title>NSU BUS | Login & Registration</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="preconnect" href="https://fonts.gstatic.com" />
+    <link rel="preconnect" href="https://firebasestorage.googleapis.com" />
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+    <link href="css/aspect-elements.css" rel="stylesheet" />
+    <link href="css/aspect-style.css" rel="stylesheet" />
 </head>
-<body>
-    <div class="wrapper">
-        <nav class="nav">
-            <div class="nav-logo">
-                <p>NSU-BUS</p>
+<body id="root-container" style="margin: 0;" onload="initAspectJS()">
+    <div data-element-behavior="menu" id="nav-id-0-" data-element-type="section">
+        <div id="container-id-1-" data-element-type="container">
+            <div id="container-id-12-" data-element-type="container">
+                <a id="button-id-15-" data-element-type="button"><span id="element-id-16-">NSU-BUS</span></a>
+                <a id="button-id-20-" data-element-type="button" href="all-buses"><span id="element-id-19-">Add Bus</span></a>
+                <a id="button-id-67-" data-element-type="button" href="bus-route"><span id="element-id-66-">Bus Route</span></a>
             </div>
-            <div class="nav-menu" id="navMenu">
-                <ul>
-                    <li><a href="#" class="link active">Home</a></li>
-                    <li><a href="#" class="link">All Buses</a></li>
-                    <li><a href="#" class="link">Bus Route</a></li>
-                    <li><a href="#" class="link">About</a></li>
-                </ul>
+            <div id="container-id-14-" data-element-type="container">
+                <a id="button-id-21-" data-element-type="button" href="#"><span id="element-id-22-">Sign-In</span></a>
+                <a id="button-id-79-" data-element-type="button" href="sign-up"><span id="element-id-78-">Sign-Up</span></a>
+                <a id="button-id-169-" data-element-type="button"><span id="element-id-168-">About-Us</span></a>
             </div>
-            <div class="nav-button">
-                <button class="btn white-btn" id="loginBtn" onclick="login()">Sign In</button>
-                <button class="btn" id "registerBtn" onclick="register()">Sign Up</button>
-            </div>
-            <div class="nav-menu-btn">
-                <i class="bx bx-menu" onclick="myMenuFunction()"></i>
-            </div>
-        </nav>
-        
-        <!-- Form box -->
-        <div class="form-box">
-            
-            <!-- Login form -->
-            <form action="" class="login-container" id="login">
-                <div class="top">
-                    <span>Don't have an account? <a href="#" onclick="register()">Sign Up</a></span>
-                    <header>Login</header>
-                </div>
-                <div class="input-box">
-                    <input type="text" class="input-field" placeholder="Username or User-ID">
-                    <i class="bx bx-user"></i>
-                </div>
-                <div class="input-box">
-                    <input type="password" class="input-field" placeholder="Password">
-                    <i class="bx bx-lock-alt"></i>
-                </div>
-                <div class="input-box">
-                    <input type="submit" class="submit" value="Sign In">
-                </div>
-                <div class="two-col">
-                    <div class="one">
-                        <input type="checkbox" id="login-check">
-                        <label for="login-check">Remember Me</label>
-                    </div>
-                    <div class="two">
-                        <label><a href="#">Forgot password?</a></label>
-                    </div>
-                </div>
-            </form>
-            
-            <!-- Registration form -->
-            <form action="" class="register-container" id="register">
-                <div class="top">
-                    <span>Have an account? <a href="#" onclick="login()">Login</a></span>
-                    <header>Sign Up</header>
-                </div>
-                <div class="input-box">
-                    <input type="text" class="input-field" placeholder="Username (User-ID)">
-                    <i class="bx bx-user"></i>
-                </div>
-                <div class="two-forms">
-                    <div class="input-box">
-                        <input type="text" class="input-field" placeholder="Full Name">
-                        <i class="bx bx-user"></i>
-                    </div>
-                </div>
-                <div class="input-box">
-                    <input type="text" class="input-field" placeholder="Age">
-                    <i class="bx bx-calendar"></i>
-                </div>
-                <div class="input-box">
-                    <input type="text" class="input-field" placeholder="Mobile Number">
-                    <i class="bx bx-phone"></i>
-                </div>
-                <div class="input-box">
-                    <input type="text" class="input-field" placeholder="Degree">
-                    <i class="bx bx-book-reader"></i>
-                </div>
-                <div class="input-box">
-                    <input type="text" class="input-field" placeholder="Institute">
-                    <i class="bx bx-building-house"></i>
-                </div>
-                <div class="input-box">
-                    <input type="text" class="input-field" placeholder="Year of Study">
-                    <i class="bx bx-calendar-star"></i>
-                </div>
-                <div class="input-box">
-                    <input type="text" class="input-field" placeholder="Profession">
-                    <i class="bx bx-briefcase"></i>
-                </div>
-                <div class="input-box">
-                    <input type="submit" class="submit" value="Register">
-                </div>
-                <div class="two-col">
-                    <div class="one">
-                        <input type="checkbox" id="register-check">
-                        <label for="register-check">Remember Me</label>
-                    </div>
-                    <div class="two">
-                        <label><a href="#">Terms & conditions</a></label>
-                    </div>
-                </div>
-            </form>
         </div>
     </div>
-
-    <script>
-        function myMenuFunction() {
-            var i = document.getElementById("navMenu");
-
-            if (i.className === "nav-menu") {
-                i.className += " responsive";
-            } else {
-                i.className = "nav-menu";
-            }
-        }
-    </script>
-
-    <script>
-        var a = document.getElementById("loginBtn");
-        var b = document.getElementById("registerBtn");
-        var x = document.getElementById("login");
-        var y = document.getElementById("register");
-
-        function login() {
-            x.style.left = "4px";
-            y.style.right = "-520px";
-            a.className += " white-btn";
-            b.className = "btn";
-            x.style.opacity = 1;
-            y.style.opacity = 0;
-        }
-
-        function register() {
-            x.style.left = "-510px";
-            y.style.right = "5px";
-            a.className = "btn";
-            b.className += " white-btn";
-            x.style.opacity = 0;
-            y.style.opacity = 1;
-        }
-    </script>
+    <div id="content-container">
+        <div id="container-id-35-" data-element-type="container">
+            <div id="container-id-37-" data-element-type="container">
+                <p id="text-id-38-" data-element-type="text">Aspect</p>
+                <div id="container-id-39-" data-element-type="container">
+                    <a id="button-id-40-" data-element-type="button" href="https://aspect.app"><span id="element-id-41-">Home</span></a>
+                    <a id="button-id-42-" data-element-type="button" href="https://aspect.app"><span id="element-id-43-">About</span></a>
+                </div>
+            </div>
+            <div id="container-id-44-" data-element-type="container">
+                <p id="text-id-45-" data-element-type="text">Product</p>
+                <div id="container-id-46-" data-element-type="container">
+                    <a id="button-id-47-" data-element-type="button" href="https://aspect.app/layouts"><span id="element-id-48-">Features</span></a>
+                    <a id="button-id-49-" data-element-type="button" href="https://aspect.app/layouts"><span id="element-id-50-">Layouts</span></a>
+                </div>
+            </div>
+            <div id="container-id-51-" data-element-type="container">
+                <p id="text-id-52-" data-element-type="text">Contact</p>
+                <div id="container-id-53-" data-element-type="container">
+                    <a id="button-id-54-" data-element-type="button" href="mailto:founders@aspect.app"><span id="element-id-55-">Email</span></a>
+                    <a id="button-id-56-" data-element-type="button" href="https://twitter.com/aspectdotapp"><span id="element-id-57-">Twitter</span></a>
+                </div>
+            </div>
+        </div>
+        <a id="image-id-32-" data-element-type="image"><img id="element-id-33-" src="img/-project-image-0.webp"></a>
+        <div data-element-behavior="form" id="form-id-139-" data-element-type="section">
+            <form action="include/server.php" id="container-id-134-" data-element-type="container" method="post" >
+                <p id="text-id-140-" data-element-type="text"><span id="element-id-143-">Don't have</span> an Account?</p>
+                <a id="button-id-147-" data-element-type="button"><span id="element-id-146-">Login</span></a>
+                <input id="input-id-136-" name="user_id" data-element-type="input" required="true" placeholder="Username">
+                <input id="input-id-149-" name="pwds" data-element-type="input" type="password" required="true" placeholder="Password">
+                
+                <button id="button-id-150-" data-element-type="button" type="submit" name="submit"><span id="element-id-437-">Sign In</span></button>
+                <a id="button-id-141-" data-element-type="button" href="sign-up"><span id="element-id-142-">Sign Up</span></a>
+            </form>
+        </div>
+        <div id="section-id-210-" data-element-type="section">
+            <div id="container-id-182-" data-element-type="container">
+                <div id="container-id-183-" data-element-type="container">
+                    <p id="text-id-184-" data-element-type="text">Economy</p>
+                    <a id="button-id-190-" data-element-type="button"><span id="element-id-191-">Dhaka To Cox's Bazar</span></a>
+                    <div id="container-id-186-" data-element-type="container">
+                        <p id="text-id-187-" data-element-type="text">1200Tk</p>
+                    </div>
+                </div>
+                <div id="container-id-192-" data-element-type="container">
+                    <p id="text-id-193-" data-element-type="text">Premium</p>
+                    <a id="button-id-199-" data-element-type="button"><span id="element-id-200-">Dhaka To Cox's Bazar</span></a>
+                    <div id="container-id-195-" data-element-type="container">
+                        <p id="text-id-196-" data-element-type="text">1700Tk</p>
+                    </div>
+                </div>
+                <div id="container-id-201-" data-element-type="container">
+                    <p id="text-id-202-" data-element-type="text">Premium Pro</p>
+                    <a id="button-id-208-" data-element-type="button"><span id="element-id-209-">Dhaka To Cox's Bazar</span></a>
+                    <div id="container-id-204-" data-element-type="container">
+                        <p id="text-id-205-" data-element-type="text">2250Tk</p>
+                    </div>
+                </div>
+                <div id="container-id-223-" data-element-type="container">
+                    <p id="text-id-218-" data-element-type="text">Premium Pro Ultra</p>
+                    <a id="button-id-219-" data-element-type="button"><span id="element-id-220-">Dhaka To Cox's Bazar</span></a>
+                    <div id="container-id-221-" data-element-type="container">
+                        <p id="text-id-222-" data-element-type="text">3000Tk</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <p id="text-id-211-" data-element-type="text">Bus Fare List</p>
+        <div id="section-id-260-" data-element-type="section">
+            <div id="container-id-236-" data-element-type="container">
+                <div id="container-id-237-" data-element-type="container">
+                    <div id="container-id-238-" data-element-type="container">
+                        <p id="text-id-239-" data-element-type="text">NSU Bus&nbsp;</p>
+                        <div id="container-id-240-" data-element-type="container">
+                            <a id="button-id-241-" data-element-type="button" href="https://aspect.app"><span id="element-id-242-">Home</span></a>
+                            <a id="button-id-243-" data-element-type="button" href="https://aspect.app"><span id="element-id-244-">About</span></a>
+                        </div>
+                    </div>
+                    <div id="container-id-245-" data-element-type="container">
+                        <p id="text-id-246-" data-element-type="text">Product</p>
+                        <div id="container-id-247-" data-element-type="container">
+                            <a id="button-id-248-" data-element-type="button" href="https://aspect.app/layouts"><span id="element-id-249-">Features</span></a>
+                            <a id="button-id-250-" data-element-type="button" href="https://aspect.app/layouts"><span id="element-id-251-">Layouts</span></a>
+                        </div>
+                    </div>
+                    <div id="container-id-252-" data-element-type="container">
+                        <p id="text-id-253-" data-element-type="text">Contact</p>
+                        <div id="container-id-254-" data-element-type="container">
+                            <a id="button-id-255-" data-element-type="button" href="mailto:founders@aspect.app"><span id="element-id-256-">Email</span></a>
+                            <a id="button-id-257-" data-element-type="button" href="https://twitter.com/aspectdotapp"><span id="element-id-258-">Twitter</span></a>
+                        </div>
+                    </div>
+                </div>
+                <p id="text-id-259-" data-element-type="text">© 2023 North South University, Inc. All rights reserved.</p>
+            </div>
+        </div>
+    </div>
+    <script src="script.js"></script>
 </body>
 </html>
